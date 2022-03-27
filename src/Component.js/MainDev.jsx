1e3f2e3365ea86d8afb1a51987da89342bb8735f
ameedasmah1 <ameed.a@averotech.com>
2022-03-27 14:36:40 +0300

    modify home page

--- src/Component.js/MainDev.jsx
+++ src/Component.js/MainDev.jsx
@@ -1,73 +1,93 @@
 import React from "react";
 
 const MainDev = () => {
+  const text = {
+    ramdan: "رمضان كريم",
+    ramdanText: ` طوال خمسة عشرة عاما حرصت اف اكس دي دي على مشاركة عملائها الكرام في
+الوطن العربي بالاحتفال بشهر رمضان الكريم بعروض حصرية مميزه هذا العام
+يسعدنا ان نشارككم أقوى عروضنا بجوائز مالية نقدية تتجاوز 30 ألف دولار
+واجمالي مكافئات مالية غير مسبوقة تتجاوز اكثر من 100 ألف دولار بالأضافة
+الى الجوائز العينية القيمة الفخمة`,
+    ramdanGift: [
+      "  جوائز مالية نقدية باكثر من ",
+      "   30 الف دولار",
+      " اجمالي بوانص تصل الى",
+      "100 الف دولار",
+    ],
+  };
   return (
     <div
-      className="main flex lg:flex-row justify-between lg:items-start md:flex-col  md:justify-end md:items-end md:mt-8 md:mb-12  lg:gap-44 lg:mt-40 "
+      className="main font-Tajawal flex lg:flex-row justify-between lg:items-start md:flex-col  md:justify-end md:items-end md:mt-8 md:mb-12  lg:gap-44 pt-44 "
       id="main"
       style={{ maxWidth: 828 }}
     >
       <div className="flex flex-col " style={{ maxWidth: 315, minWidth: 315 }}>
         <p
           className="text-3xl text-right font-bold text-white leading-10 "
-          style={{ letterSpacing: -0.15, }}
+          style={{ letterSpacing: -0.15 }}
         >
-          رمضان كريم
+          {text.ramdan}
         </p>
         <p
           className="text-white  font-bold text-right pt-3 sm:mb-16 "
-          style={{ letterspacing: -0.15,fontSize:18 }}
+          style={{ letterspacing: -0.15, fontSize: 18 }}
         >
-          طوال خمسة عشرة عاما حرصت اف اكس دي دي على مشاركة عملائها الكرام
-          في الوطن العربي بالاحتفال بشهر رمضان الكريم بعروض حصرية مميزه هذا
-          العام يسعدنا ان نشارككم أقوى عروضنا بجوائز مالية نقدية تتجاوز 30 ألف
-          دولار واجمالي مكافئات مالية غير مسبوقة تتجاوز اكثر من 100 ألف دولار
-         بالأضافة الى الجوائز العينية القيمة الفخمة
+          {text.ramdanText}
         </p>
       </div>
-      <div className="flex flex-col align-middle" style={{ minWidth: 240,maxWidth:320 }}>
+      <div
+        className="flex flex-col align-middle"
+        style={{ minWidth: 250, maxWidth: 320 }}
+      >
         <p
           className="text-right text-white  font-bold"
-          style={{ letterspacing: -0.15,  fontSize: 36 }}
+          style={{ letterspacing: -0.15, fontSize: 38 }}
         >
+          {" "}
           <span
             className="font-Montserrat text-amber-400"
-            style={{ letterspacing: -0.15,  fontSize: 37 }}
+            style={{ letterspacing: -0.15, fontSize: 38 }}
           >
-            FXDD
+            FXDD{" "}
           </span>
           عروض
           <br />
           لشهر رمضان كريم
         </p>
-        <div className="flex flex-row justify-end pt-1 items-center">
-          <p
-            className=" text-white font-normal leading-10 pr-1.5 mb-1 "
-            style={{ letterSpacing: -0.15, fontSize: 17.5 }}
-          >
-            جوائز مالية نقدية باكثر من <span className="font-bold " style={{ fontWeight:1000,fontSize:17.5 }}>30 الف دولار</span>
-          </p>
+
+        <p
+          className="point flex justify-end pt-2.5  text-white font-normal"
+          style={{ letterSpacing: -0.15, fontSize: 15.5 }}
+        >
           <span
-            className="h-2 w-2 mb-1 bg-amber-400 inline-block "
-            style={{ borderRadius: "50%" }}
-          ></span>
-        </div>
+            className="font-bold "
+            style={{ fontWeight: 1000, fontSize: 17.5 }}
+          >
+            {text.ramdanGift[1]}
+          </span>
+          {text.ramdanGift[0]}
+        </p>
+
         <div className="flex flex-row justify-end items-center -mt-3">
           <p
-            className=" mt-1.5 text-white font-normal leading-10 pr-1.5"
-            style={{ letterSpacing: -0.15, fontSize: 17.5  }}
+            className="point mt-1.5 text-white font-normal leading-10"
+            style={{ letterSpacing: -0.15, fontSize: 15.5 }}
           >
-             اجمالي بوانص تصل الى <span style={{ fontWeight:1000,fontSize:17.5 }}>100 الف دولار</span>
+            <span style={{ fontWeight: 1000, fontSize: 15.5 }}>
+              {text.ramdanGift[3]}
+            </span>
+            {text.ramdanGift[2]}
           </p>
-          <span
-            className="h-2 w-2 mt-1 bg-amber-400 inline-block"
-            style={{ borderRadius: "50%" }}
-          ></span>
         </div>
         <div className="flex flex-row justify-end mt-4">
           <a
             className="bg-white flex justify-center text-slate-800 font-bold  mr-1.5 rounded-lg text-center"
-            style={{ minWidth: 111,fontSize:14,height:42 ,alignItems:"center"}}
+            style={{
+              minWidth: 111,
+              fontSize: 14,
+              height: 42,
+              alignItems: "center",
+            }}
             href="https://www.fxddtrading.com/bm/ar/trading/deposit"
             target="_blank"
           >
@@ -77,7 +97,12 @@ const MainDev = () => {
             href="https://fxdemo.fxdd.com/olreg/applyonline/LiveRegistrationInfoAction?ibrokerCode=FXDD_L000B&languageId=ar_SA"
             target="_blank"
             className="bg-transparent flex justify-center hover:bg-amber-400 font-bold text-white max-w-xs hover:text-white px-4 border text-center border-white items-center hover:border-transparent rounded-lg"
-            style={{ minWidth: 145,fontSize:14,height:42,alignItems:"center" }}
+            style={{
+              minWidth: 145,
+              fontSize: 14,
+              height: 42,
+              alignItems: "center",
+            }}
           >
             افتح حساب الان
           </a>
